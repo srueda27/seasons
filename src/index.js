62e98c933d8dcbabf8f25bcdfe83c40bc4b0d2c4
Santiago Rueda Ortiz <srueda27@gmail.com>
2022-02-05 19:35:46 -0500

    Create constructor method and initialize state

--- src/index.js
+++ src/index.js
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom';
 import SeasonDisplay from './components/SeasonDisplay';
 
 class App extends React.Component {
+  //Optional method, runs firts when the component is being created
+  //Allows us to initialize state
+  constructor(props) {
+    super(props);
+
+    this.state = { lat: null };
+  }
+
+  //React says we have to define render!!!
   render() {
     window.navigator.geolocation.getCurrentPosition(
       position => console.log(position),
