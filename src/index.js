329149c375b642d4d2914da2175b649f92dd3537
Santiago Rueda Ortiz <srueda27@gmail.com>
2022-02-05 19:59:46 -0500

    Conditional rendering

--- src/index.js
+++ src/index.js
@@ -27,11 +27,25 @@ class App extends React.Component {
 
   //React says we have to define render!!!
   render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return (
+        <div>
+          Error: {this.state.errorMessage}
+        </div>
+      );
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return (
+        <div>
+          Latitude: {this.state.lat}
+        </div>
+      );
+    }
+
     return (
       <div>
-        Latitude: {this.state.lat}
-        <br />
-        Error: {this.state.errorMessage}
+        Loading!
       </div>
     );
   }
