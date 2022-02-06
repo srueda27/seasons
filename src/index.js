1feb792bd1bda7822820de5c6f5d82fa72195c32
Santiago Rueda Ortiz <srueda27@gmail.com>
2022-02-05 19:55:45 -0500

    Use setState to re-render a class component

--- src/index.js
+++ src/index.js
@@ -8,16 +8,32 @@ class App extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = { lat: null };
+    // THIS IS THE ONLY TIME we do direct assignment to this.state
+    this.state = { lat: null, errorMessage: '' };
+
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({
+          lat: position.coords.latitude
+        });
+      },
+      err => {
+        this.setState({
+          errorMessage: err.message
+        });
+      }
+    );
   }
 
   //React says we have to define render!!!
   render() {
-    window.navigator.geolocation.getCurrentPosition(
-      position => console.log(position),
-      err => console.log(err)
+    return (
+      <div>
+        Latitude: {this.state.lat}
+        <br />
+        Error: {this.state.errorMessage}
+      </div>
     );
-    return <div>Latitude: </div>;
   }
 }
 
