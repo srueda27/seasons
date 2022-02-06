a6ea687f6d576f007c0770a4cd285404c3b7d7d7
Santiago Rueda Ortiz <srueda27@gmail.com>
2022-02-05 20:16:56 -0500

    Refactor code with lifecycle methods

--- src/index.js
+++ src/index.js
@@ -10,22 +10,25 @@ class App extends React.Component {
 
     // THIS IS THE ONLY TIME we do direct assignment to this.state
     this.state = { lat: null, errorMessage: '' };
+  }
 
+  //First time the render method is used
+  componentDidMount() {
+    console.log('My component was rendered to the screen');
+    
     window.navigator.geolocation.getCurrentPosition(
-      position => {
-        this.setState({
-          lat: position.coords.latitude
-        });
-      },
-      err => {
-        this.setState({
-          errorMessage: err.message
-        });
-      }
+      position => this.setState( { lat: position.coords.latitude }),
+      err => this.setState({ errorMessage: err.message })
     );
   }
 
+  //Every time the render method is used
+  componentDidUpdate() {
+    console.log('My component was just updated - it rerendered')
+  }
+
   //React says we have to define render!!!
+  //Only to return jsx NOTHING ELSE
   render() {
     if (this.state.errorMessage && !this.state.lat) {
       return (
